ff7c1f8a7630753dcd5036e7e40a5a8b8d703244
Rukin Danil <rukin2018d@yandex.ru>
2022-03-09 17:41:06 +0700

    Сервисы начало

---
+++ GibddApp.Core/Data/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GibddApp.Core.Data
+{
+    public class Account
+    {
+        public int Id { get; private set; }
+        public Role Role { get; private set; }
+    }
+}
