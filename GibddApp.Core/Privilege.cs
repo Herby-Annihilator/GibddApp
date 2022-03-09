c938ff7a64dede1b95e5b4b155ae1020682c94df
Rukin Danil <rukin2018d@yandex.ru>
2022-03-10 00:44:55 +0700

    Привилегии и роли

---
+++ GibddApp.Core/Privilege.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GibddApp.Core
+{
+    /// <summary>
+    /// Privilege. Values of a power of two are allowed 
+    /// </summary>
+    [Flags]
+    public enum Privilege : uint
+    {
+        None = 0,
+
+        AddUser = 1,
+        RemoveUser = 2,
+        EditUserData = 4,
+
+        AddCar = 8,
+        RemoveCar = 16,
+        EditCarData = 32,
+
+        CreateProtocol = 64,
+        RemoveProtocol = 128,
+        EditProtocolData = 256,
+
+        CreateAccount = 512,
+        RemoveAccount = 1024,
+        EditAccountData = 2048,
+
+        Login = 4096,
+        Logout = 8192,
+
+        EditHelpInformation = 16384,
+
+        
+        // Add additional privileges
+
+        All = 0xFFFFFFFF
+    }
+}
---
+++ GibddApp.Core/Role.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GibddApp.Core
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Privilege Privilege => _privilege;
+        private Privilege _privilege = Privilege.None;
+
+        public void AddPrivilege(Privilege privilege) => _privilege |= privilege;
+
+        public void RemovePrivilege(Privilege privilege)
+        {
+            Privilege mask = ~privilege;
+            _privilege &= mask;
+        }
+
+        public bool ContainsPrivilege(Privilege privilege) => (_privilege &= privilege) == privilege;
+    }
+}
