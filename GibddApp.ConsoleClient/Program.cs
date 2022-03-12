6203a666409196deb69ab0b30be6cb9c360a1997
Rukin Danil <rukin2018d@yandex.ru>
2022-03-12 20:56:50 +0700

    Команды ADO.NET

--- GibddApp.ConsoleClient/Program.cs
+++ GibddApp.ConsoleClient/Program.cs
@@ -13,8 +13,24 @@ namespace GibddApp.ConsoleClient
         public static void Main(string[] args)
         {
             try
-            {                 
-                ConnectToPostgres();
+            {
+                NpgsqlConnection connection = new NpgsqlConnection(DefaultConnectionString());
+                Task taskCreate = CreateAddressTableAsync(connection);
+                Console.WriteLine("***********Main thread*********");
+                taskCreate.Wait();
+
+                Task taskInsert = InsertRecordsToAddressTable(5, connection);
+                Console.WriteLine("Waiting for inserting data");
+                taskInsert.Wait();
+
+                Task taskUpdate = UpdateCountrysInAddressTable("Russian Federation", "Rissia", connection);
+                Console.WriteLine("Waiting for updating data");
+                taskUpdate.Wait();
+
+                Task taskDelete = DeleteAddressWithCityName("Barnaul", connection);
+                Console.WriteLine("Waiting for deleting data");
+                taskDelete.Wait();
+
             }
             catch (Exception ex)
             {
@@ -23,7 +39,7 @@ namespace GibddApp.ConsoleClient
             }
         }
 
-        static void ConnectToPostgres()
+        static void CheckConnectionAndCloseIt()
         {
             Console.WriteLine("Connecting to PostgreSQL Pro...");
             NpgsqlConnection connection = new NpgsqlConnection(DefaultConnectionString());
@@ -36,7 +52,7 @@ namespace GibddApp.ConsoleClient
 
         static string DefaultConnectionString()
         {
-            string host = "192.168.1.123";
+            string host = "192.168.1.134";
             string database = "postgres";
             string user = "postgres";
             string password = "qwerty";
@@ -54,5 +70,106 @@ namespace GibddApp.ConsoleClient
 
         static string ConnectionString(string host, string user, string password, string database, int port) =>
             $"Host={host};Port={port};Database={database};Username={user};Password={password};";
+
+        static async Task CreateAddressTableAsync(NpgsqlConnection connection)
+        {
+            try
+            {
+                await connection.OpenAsync();
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    throw new Exception("Connection was not opened!");
+                }
+                Console.WriteLine("Connection opened");
+                NpgsqlCommand command = new NpgsqlCommand();
+                
+                command.CommandText = "create table if not exists address (" +
+                    "id serial not null CONSTRAINT firstkey PRIMARY KEY," +
+                    "country_name text not null," +
+                    "region_name text not null," +
+                    "city_name text not null," +
+                    "street_name text not null," +
+                    "house_number integer" +
+                    ")" +
+                    "with (autovacuum_enabled=true)";
+                command.CommandType = System.Data.CommandType.Text;
+                command.Connection = connection;
+                int result = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("Result: " + result);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ExceptionType: {ex.GetType().Name}");
+                Console.WriteLine($"Text: {ex.Message}");
+                Console.ReadKey(true);
+            }
+            finally
+            {
+                await connection.CloseAsync();
+                if (connection.FullState == System.Data.ConnectionState.Closed)
+                    Console.WriteLine("Connection closed");
+                else
+                    Console.WriteLine("Connection is not closed. Problem.....");
+            }
+        }
+
+        static async Task InsertRecordsToAddressTable(int countOfRecords, NpgsqlConnection connection)
+        {
+            Console.WriteLine($"{DateTime.Now}: Openinig connection...");
+            await connection.OpenAsync();
+
+            Console.WriteLine($"{DateTime.Now}: Connection: {connection.State}");
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = connection;
+            int result;
+            for (int i = 0; i < countOfRecords; i++)
+            {
+                command.CommandText = $"insert into address (country_name, region_name, city_name, street_name, house_number)" +
+                $" values ('Rissia', 'Altai region', 'Barnaul', 'Lenina', '{i}')";
+                result = command.ExecuteNonQuery();
+                Console.WriteLine($"Result: {result}");
+            }
+            Console.WriteLine($"{DateTime.Now}: Closing connection...");
+            await connection.CloseAsync();
+            Console.WriteLine($"{DateTime.Now}: Connection: {connection.State}");
+        }
+
+        static async Task UpdateCountrysInAddressTable(string country_name, string old_name, NpgsqlConnection connection)
+        {
+            Console.WriteLine($"{DateTime.Now}: Openinig connection...");
+            await connection.OpenAsync();
+
+            Console.WriteLine($"{DateTime.Now}: Connection:  {connection.State}");
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = connection;
+            int result;
+            command.CommandText = $"update address set country_name = '{country_name}' where country_name = '{old_name}'";
+            result = await command.ExecuteNonQueryAsync();
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine($"{DateTime.Now}: Closing connection...");
+            await connection.CloseAsync();
+            Console.WriteLine($"{DateTime.Now}: Connection: {connection.State}");
+        }
+
+        static async Task DeleteAddressWithCityName(string city_name, NpgsqlConnection connection)
+        {
+            Console.WriteLine("Deleting data");
+            Console.WriteLine($"{DateTime.Now}: Openinig connection...");
+            await connection.OpenAsync();
+
+            Console.WriteLine($"{DateTime.Now}: Connection: {connection.State}");
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.Connection = connection;
+            int result;
+            command.CommandText = $"delete from address where city_name = '{city_name}';";
+            result = await command.ExecuteNonQueryAsync();
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine($"{DateTime.Now}: Closing connection...");
+            await connection.CloseAsync();
+            Console.WriteLine($"{DateTime.Now}: Connection: {connection.State}");
+        }
     }
 }
